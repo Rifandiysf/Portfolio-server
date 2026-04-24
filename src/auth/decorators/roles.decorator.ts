28a520040643aca36a28cb12aca80b49778e2158
Rifandiysf <rifandiyusuf47@gmail.com>
2026-04-24 21:46:58 +0700

    fix(roles): correct AdminOnly decorator to use array for roles

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -3,6 +3,4 @@ import { Role } from 'prisma/generated/enums';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
-
-export const ROLES_ADMIN_KEY = 'requiresAdmin';
-export const AdminOnly = () => SetMetadata(ROLES_KEY, true);
+export const AdminOnly = () => SetMetadata(ROLES_KEY, [Role.ADMIN]);
